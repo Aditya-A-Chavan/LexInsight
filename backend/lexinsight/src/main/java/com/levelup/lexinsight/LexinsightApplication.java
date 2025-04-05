cc2c5e30e40283c3610a207aba29a7bdc92cb2f2
Suja16 <sujal16@somaiya.edu>
2025-04-05 18:17:42 +0530

    backend int2

--- backend/lexinsight/src/main/java/com/levelup/lexinsight/LexinsightApplication.java
+++ backend/lexinsight/src/main/java/com/levelup/lexinsight/LexinsightApplication.java
@@ -1,4 +1,4 @@
-package com.level_up.lexinsight;
+package com.levelup.lexinsight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/lexinsight/src/test/java/com/levelup/lexinsight/LexinsightApplicationTests.java
+++ backend/lexinsight/src/test/java/com/levelup/lexinsight/LexinsightApplicationTests.java
@@ -1,4 +1,4 @@
-package com.level_up.lexinsight;
+package com.levelup.lexinsight;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
