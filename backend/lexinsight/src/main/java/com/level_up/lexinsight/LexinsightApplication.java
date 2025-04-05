8422ece91af5c6015126725dd29baa80b5060feb
Suja16 <sujal16@somaiya.edu>
2025-04-05 17:52:35 +0530

    backend ini

---
+++ backend/lexinsight/src/main/java/com/level_up/lexinsight/LexinsightApplication.java
@@ -0,0 +1,13 @@
+package com.level_up.lexinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexinsightApplication.class, args);
+	}
+
+}
---
+++ backend/lexinsight/src/test/java/com/level_up/lexinsight/LexinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level_up.lexinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
